6acb2c9a4c9a435f4368674ba7bf292a3c5b3fcf
kavijarr <kavijakumuditha12@gmail.com>
2024-07-21 00:09:01 +0530

    company registration with file upload

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -3,14 +3,56 @@ import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { ContactUsComponent } from "../../components/contact-us/contact-us.component";
-
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink, NavbarComponent, FooterComponent, ContactUsComponent],
+  imports: [RouterLink, NavbarComponent, FooterComponent, ContactUsComponent, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  selectedFile: File | null = null;
+  company = {
+    username: '',
+    password: '',
+    company_name: '',
+    br: '',
+    email: '',
+    address: '',
+    phone_number: ''
+  };
+
+  constructor(private http: HttpClient){}
+
+  onFileChange(event:any){
+    this.selectedFile = event.target.files[0];
+  }
+
+  onSubmit(){
+    if (this.selectedFile) {
+      const formData = new FormData();
+      formData.append('image', this.selectedFile);
+      formData.append('username', this.company.username);
+      formData.append('password', this.company.password);
+      formData.append('company_name', this.company.company_name);
+      formData.append('br', this.company.br);
+      formData.append('email', this.company.email);
+      formData.append('address', this.company.address);
+      formData.append('phone_number', this.company.phone_number);
+
+      this.http.post('http://localhost:3000/company', formData).subscribe(
+        (response) => {
+          console.log('Upload successful', response);
+        },
+        (error) => {
+          console.error('Upload failed', error);
+        }
+      );
+    } else {
+      console.error('No file selected');
+    }
+  }
 }
